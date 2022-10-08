fe75c5b8d663da447fc225c59c3fdfdc16a1fee1
Aryan <aryanyash999@gmail.com>
2022-10-08 18:57:18 +0530

    bug fix

--- public/App.js
+++ public/App.js
@@ -11,23 +11,6 @@ particlesJS("particles-js", {
 		color: {
 			value: ["#BD10E0", "#B8E986", "#50E3C2", "#FFD300", "#E86363"],
 		},
-		shape: {
-			type: "circle",
-			stroke: {
-				width: 0,
-				color: "#b6b2b2",
-			},
-		},
-		opacity: {
-			value: 0.5211089197812949,
-			random: false,
-			anim: {
-				enable: true,
-				speed: 4,
-				opacity_min: 0.1,
-				sync: false,
-			},
-		},
 		size: {
 			value: 8.5,
 			random: true,
@@ -37,13 +20,6 @@ particlesJS("particles-js", {
 				size_min: 0.1,
 				sync: true,
 			},
-		},
-		line_linked: {
-			enable: true,
-			distance: 200,
-			color: "#c8c8c8",
-			opacity: 0.4,
-			width: 1,
 		},
 		move: {
 			enable: true,
@@ -55,51 +31,11 @@ particlesJS("particles-js", {
 			bounce: false,
 			attract: {
 				enable: false,
-				rotateX: 600,
+				rotateX: 900,
 				rotateY: 1200,
 			},
 		},
 	},
-	interactivity: {
-		detect_on: "canvas",
-		events: {
-			onhover: {
-				enable: false,
-				mode: "repulse",
-			},
-			onclick: {
-				enable: false,
-				mode: "push",
-			},
-			resize: true,
-		},
-		modes: {
-			grab: {
-				distance: 400,
-				line_linked: {
-					opacity: 1,
-				},
-			},
-			bubble: {
-				distance: 400,
-				size: 40,
-				duration: 2,
-				opacity: 8,
-				speed: 3,
-			},
-			repulse: {
-				distance: 200,
-				duration: 0.4,
-			},
-			push: {
-				particles_nb: 4,
-			},
-			remove: {
-				particles_nb: 2,
-			},
-		},
-	},
-	retina_detect: true,
 });
 
 // typing animation
@@ -141,13 +77,12 @@ document.addEventListener("mouseout", () => {
 	cursor.style.display = "none";
 });
 
-
 // pwa
 
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		navigator.serviceWorker
-			.register("/sw.js")
+			.register("sw.js")
 			.then((res) => console.log("service worker registered"))
 			.catch((err) => console.log("service worker not registered", err));
 	});
